0e199a27b8dd3518e0b9327d358b8bdc804abdcd
schiligoski <159257519+schilgoski@users.noreply.github.com>
2024-10-30 08:51:52 -0300

    Add files via upload

--- InformacoesGlobais.js
+++ InformacoesGlobais.js
@@ -1,9 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
@@ -19,4 +16,3 @@ async function vizualizarInformacoesGlobais() {
 }
 
 vizualizarInformacoesGlobais()
-
--- common.js
+++ common.js
@@ -1,12 +1,30 @@
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
     return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
 const tickConfig = {
-    family: getCSS('--font'),
+    color: getCSS('--primary-color'),
     size: 16,
-    color: getCSS('--primary-color')
+    family: getCSS('--font')
+    
+    }
+
+    function criarGrafico(data, layout){
+        const grafico = document.createElement('div')
+        grafico.className = 'grafico'
+        document.getElementsById('graficos-container').appenChid(grafico)
+        const config = {
+            responsive: true,
+            displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config)
+}
+function incluirTexto(texto){
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
 }
 
-export { getCSS, tickConfig }
+export { getCSS, tickConfig, criarGrafico, incluirTexto}
