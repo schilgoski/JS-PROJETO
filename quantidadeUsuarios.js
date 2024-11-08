34721372114539d5a8306ad81fbb3256973cbf03
schiligoski <159257519+schilgoski@users.noreply.github.com>
2024-11-08 10:50:25 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,23 +1,56 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+  import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-      
     const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-    
+    const quantidadeDeUsuarios = Object.values(dados)
+
     const data = [
         {
-          x: nomeDasRedes,
-          y: quantidadeUsuarios,
-          type: 'bar'
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
-      ]
-      
-      const grafico = document.createElement('div')
-      grafico.className = 'grafico'
-      document.getElementById('grafico-container').appendChild(grafico)
-      Plotly.newPlot(grafico, data)
-  }
-  
-  quantidadeUsuarios()
+    ]
+
+    const laytout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+    }
+
+    criarGrafico(data, layout)
+}
+
+quantidadeUsuariosPorRede()
